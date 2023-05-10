1bcefc8c6cea7d87c0ddf8a0e265055ea7fbe69b
Jonathan LEVEQUE <jleveque@scheiber.fr>
2023-05-10 11:38:25 +0200

    Chapitre7-8-9

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,4 +1,7 @@
+import { transition } from '@angular/animations';
+import { UserService } from './../shared/services/user.service';
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user-list',
@@ -7,11 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserListComponent implements OnInit {
   public users!: string[];
+  public newUsername: string = '';
+  public subscription: Subscription = new Subscription();
 
-  constructor() // il faut probablement injecter un service ici !
-  {}
+  constructor(
+    private userService: UserService // il faut probablement injecter un service ici !
+  ) {}
 
   ngOnInit() {
     // il faut initialiser les users ici avec le service
+    this.subscription.add(
+      this.userService.users$.subscribe((users) => (this.users = users))
+    );
   }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  // public addUser(): void {
+  //   this.userService.addUser(this.newUsername);
+  //   this.newUsername = '';
+  // }
 }
